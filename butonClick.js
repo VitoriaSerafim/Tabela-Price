83273b1f9b9436439754a6dbb7d533668239eb64
vitria de souza serafim <vitoriass@dcc.ufrj.br>
2023-11-04 00:44:02 -0300

    conserto bug da entrada

--- butonClick.js
+++ butonClick.js
@@ -1,6 +1,7 @@
 // Obtém o botão e o conteúdo
 const submitButton = document.getElementById('submitButton');
 const result = document.getElementById('result');
+const goBack = document.getElementById('goBackBtn');
 
 // Define um estado inicial
 let visibilidade = false;
@@ -9,8 +10,10 @@ let visibilidade = false;
 submitButton.addEventListener('click', function () {
     if (visibilidade) {
         result.style.display = 'none'; // Oculta o conteúdo
+        goBack.style.display = 'none'; // Oculta o conteúdo
     } else {
         result.style.display = 'block'; // Torna o conteúdo visível
+        goBack.style.display = 'block'; // Torna o conteúdo visível
     }
     // Inverte o estado
     visibilidade = !visibilidade;
